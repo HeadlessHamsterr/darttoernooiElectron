23a18fa3d6d2390ca9103d10a2168e805d5024f0
Joost <joost_rietdijk@live.nl>
2021-09-09 21:36:53 +0200

    Poules toegevoegd

--- src/render.js
+++ src/render.js
@@ -3,11 +3,16 @@ let $ = require('jquery');
 var numPlayers = 0;
 var numPoules = 0;
 
+var players = [];
+var pouleA = [];
+var pouleB = [];
+var pouleC = [];
+var pouleD = [];
+
 const subBtn = document.getElementById('subBtn');
 subBtn.onclick = getGameInfo;
 
 const makePoulesBtn = document.getElementById('mkPoulesBtn');
-makePoulesBtn.onclick = makePoules;
 
 $(document.getElementById('playerInputDiv')).hide();
 $(document.getElementById('poulesDiv')).hide();
@@ -22,6 +27,7 @@ function getGameInfo(){
     //$(document.getElementById('gameSetup')).hide();
     $("div").hide();
     $(document.getElementById('playerInputDiv')).show();
+    $(document.getElementById('playerInputSubDiv')).show();
 
     var playerInputForm = document.getElementById('playerInputForm');
 
@@ -33,24 +39,173 @@ function getGameInfo(){
         playerInput.setAttribute("class", "playerInput");
         $(playerInputForm).append(playerInput);
     }
+
+    makePoulesBtn.onclick = makePoules;
 }
 
 function makePoules(){
     console.log("Making poules");
-    //$(document.getElementById('playerInputDiv')).hide();
-    //$(document.getElementById('gameSetup')).hide();
     $("div").hide();
 
-    var poulesDiv = document.getElementById('poulesDiv')
-    $(poulesDiv).show();
+    var poulesDiv = document.getElementById('poulesDiv');
 
     for(let i = 0; i < numPlayers; i++){
         var playerName = document.getElementById(`player${i}`).value;
-        var playerText = $(`<p>${playerName}</p>`)
+        players.push(playerName);
+    }
 
-        var newLine = $("<br>");
+    players.sort(function(a,b){return 0.5 - Math.random()});
+    console.log(players);
+    
+    var PLAYERS_PER_POULE = numPlayers/numPoules;
+    console.log(PLAYERS_PER_POULE);
 
-        $(poulesDiv).append(playerText);
-        $(poulesDiv).append(newLine);
+    for(let i = 0; i < numPlayers; i++){
+        if(i < PLAYERS_PER_POULE){
+            var tempArray = [players[i], i];
+            pouleA.push(tempArray);
+        }else if(PLAYERS_PER_POULE <= i && i < (2*PLAYERS_PER_POULE)){
+            var tempArray = [players[i], i];
+            pouleB.push(tempArray)
+        }else if((2*PLAYERS_PER_POULE) <= i && i < (3*PLAYERS_PER_POULE)){
+            var tempArray = [players[i], i];
+            pouleC.push(tempArray)
+        }else if((3*PLAYERS_PER_POULE) <= i && i < (4*PLAYERS_PER_POULE)){
+            var tempArray = [players[i], i];
+            pouleD.push(tempArray)
+        }
+    }
+
+    console.log(pouleA);
+    console.log(pouleB);
+    console.log(pouleC);
+    console.log(pouleD);
+
+    if(typeof pouleA !== 'undefined' && pouleA.length > 0){
+        var playerDiv = $('<div id="pouleA" class="pouleDiv"></div>');
+        var pouleHeader = $('<header class="pouleHeader"><h2>Poule A:</h2><hr/></header>');
+        var pouleTable = $('<table class="pouleTable" id="pouleATable"></table>');
+
+        $(poulesDiv).append(playerDiv);
+        $(playerDiv).append(pouleHeader);
+        $(playerDiv).append(pouleTable);
+
+        for(let i in pouleA){
+            var tableEntry = $(`<tr><td>${pouleA[i][0]}</td><td>${pouleA[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+
+        $(playerDiv).show();
+    }
+
+    if(typeof pouleB !== 'undefined' && pouleB.length > 0){
+        var playerDiv = $('<div id="pouleB" class="pouleDiv"></div>');
+        var pouleHeader = $('<header class="pouleHeader"><h2>Poule B:</h2><hr/></header>');
+        var pouleTable = $('<table class="pouleTable" id="pouleBTable"></table>');
+
+        $(poulesDiv).append(playerDiv);
+        $(playerDiv).append(pouleHeader);
+        $(playerDiv).append(pouleTable);
+
+        for(let i in pouleB){
+            var tableEntry = $(`<tr><td>${pouleB[i][0]}</td><td>${pouleB[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+
+        $(playerDiv).show();
+    }
+
+    if(typeof pouleC !== 'undefined' && pouleC.length > 0){
+        var playerDiv = $('<div id="pouleC" class="pouleDiv"></div>');
+        var pouleHeader = $('<header class="pouleHeader"><h2>Poule C:</h2><hr/></header>');
+        var pouleTable = $('<table class="pouleTable" id="pouleCTable"></table>');
+
+        $(poulesDiv).append(playerDiv);
+        $(playerDiv).append(pouleHeader);
+        $(playerDiv).append(pouleTable);
+
+        for(let i in pouleC){
+            var tableEntry = $(`<tr><td>${pouleC[i][0]}</td><td>${pouleC[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+
+        $(playerDiv).show();
+    }
+
+    if(typeof pouleD !== 'undefined' && pouleD.length > 0){
+        var playerDiv = $('<div id="pouleD" class="pouleDiv"></div>');
+        var pouleHeader = $('<header class="pouleHeader"><h2>Poule D:</h2><hr/></header>');
+        var pouleTable = $('<table class="pouleTable" id="pouleDTable"></table>');
+
+        $(poulesDiv).append(playerDiv);
+        $(playerDiv).append(pouleHeader);
+        $(playerDiv).append(pouleTable);
+
+        for(let i in pouleD){
+            var tableEntry = $(`<tr><td>${pouleD[i][0]}</td><td>${pouleD[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+
+        $(playerDiv).show();
+    }
+
+    $(poulesDiv).append($('<button id="sortPoules">Sorteer</button>'));
+    document.getElementById('sortPoules').onclick = sortPoules;
+    $(poulesDiv).show();
+}
+
+function sortPoules(){
+    console.log("Poules sorteren...");
+
+    if(typeof pouleA !== 'undefined' && pouleA.length > 0){
+        pouleA.sort(function(a,b){return b[1]-a[1]});
+        console.log(pouleA);
+        
+        var pouleTable = document.getElementById('pouleATable');
+        $(pouleTable).empty();
+
+        for(let i in pouleA){
+            var tableEntry = $(`<tr><td>${pouleA[i][0]}</td><td>${pouleA[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+    }
+
+    if(typeof pouleB !== 'undefined' && pouleB.length > 0){
+        pouleB.sort(function(a,b){return b[1]-a[1]});
+        console.log(pouleB);
+        
+        var pouleTable = document.getElementById('pouleBTable');
+        $(pouleTable).empty();
+
+        for(let i in pouleB){
+            var tableEntry = $(`<tr><td>${pouleB[i][0]}</td><td>${pouleB[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+    }
+
+    if(typeof pouleC !== 'undefined' && pouleC.length > 0){
+        pouleC.sort(function(a,b){return b[1]-a[1]});
+        console.log(pouleC);
+        
+        var pouleTable = document.getElementById('pouleCTable');
+        $(pouleTable).empty();
+
+        for(let i in pouleC){
+            var tableEntry = $(`<tr><td>${pouleC[i][0]}</td><td>${pouleC[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
+    }
+
+    if(typeof pouleD !== 'undefined' && pouleD.length > 0){
+        pouleD.sort(function(a,b){return b[1]-a[1]});
+        console.log(pouleD);
+        
+        var pouleTable = document.getElementById('pouleDTable');
+        $(pouleTable).empty();
+
+        for(let i in pouleD){
+            var tableEntry = $(`<tr><td>${pouleD[i][0]}</td><td>${pouleD[i][1]}</td></tr>`);
+            $(pouleTable).append(tableEntry);
+        }
     }
 }
