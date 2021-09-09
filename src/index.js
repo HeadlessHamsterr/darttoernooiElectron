32c9cdfbede29260c305391a8b0595b4150b4bbd
HeadlessHamsterr <joost_rietdijk@live.nl>
2021-09-09 02:47:08 +0200

    Kleine aanpassingen

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
-
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
