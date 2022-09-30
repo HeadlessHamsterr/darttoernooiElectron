059c768f0e58897a16a98095a5bd188b8571a8ad
HeadlessHamsterr <joost_rietdijk@live.nl>
2022-10-01 00:36:52 +0200

    Implemented new game data format

--- Electron/src/activeGames.js
+++ Electron/src/activeGames.js
@@ -1,4 +1,4 @@
-let {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 let $ = require('jquery');
 
 var numActiveGames = 0;
@@ -36,76 +36,78 @@ function updateGames(data, infoType){
     console.log(data);
     switch(infoType){
         case 0:
-            document.getElementById(`activeLeg${data[0]}1`).innerHTML = data[2];
-            document.getElementById(`activeLeg${data[0]}2`).innerHTML = data[4];
-            document.getElementById(`activeScore${data[0]}1`).innerHTML = data[1];
-            document.getElementById(`activeScore${data[0]}2`).innerHTML = data[3];
-            document.getElementById(`activeDarts${data[0]}1`).innerHTML = data[7];
-            document.getElementById(`activeDarts${data[0]}2`).innerHTML = data[8];
+            document.getElementById(`activeLeg${data[0].gameID}1`).innerHTML = data[0].player1LegsWon;
+            document.getElementById(`activeLeg${data[0].gameID}2`).innerHTML = data[0].player2LegsWon;
+            document.getElementById(`activeScore${data[0].gameID}1`).innerHTML = data[0].player1Score;
+            document.getElementById(`activeScore${data[0].gameID}2`).innerHTML = data[0].player2Score;
+            document.getElementById(`activeDarts${data[0].gameID}1`).innerHTML = data[0].player1DartsThrown;
+            document.getElementById(`activeDarts${data[0].gameID}2`).innerHTML = data[0].player2DartsThrown;
 
-            if(data[6] == '0'){
-                document.getElementById(`activePlayer${data[0]}1`).style.color = 'green';
-                document.getElementById(`activePlayer${data[0]}2`).style.color = 'white';
+            if(data[0].startingPlayer == '0'){
+                document.getElementById(`activePlayer${data[0].gameID}1`).style.color = 'green';
+                document.getElementById(`activePlayer${data[0].gameID}2`).style.color = 'white';
             }else{
-                document.getElementById(`activePlayer${data[0]}1`).style.color = 'white';
-                document.getElementById(`activePlayer${data[0]}2`).style.color = 'green';
+                document.getElementById(`activePlayer${data[0].gameID}1`).style.color = 'white';
+                document.getElementById(`activePlayer${data[0].gameID}2`).style.color = 'green';
             }
         
-            if(data[5] == 'true'){
-                document.getElementById(`${data[0]}TurnArrow`).style.transform = 'rotate(90deg)';
+            if(data[0].player1Turn == 'true'){
+                document.getElementById(`${data[0].gameID}TurnArrow`).style.transform = 'rotate(90deg)';
             }else{
-                document.getElementById(`${data[0]}TurnArrow`).style.transform = 'rotate(270deg)';
+                document.getElementById(`${data[0].gameID}TurnArrow`).style.transform = 'rotate(270deg)';
             }
         
-            if(data[1] <= 170 && data[1] > 0){
-                document.getElementById(`out${data[0]}1`).innerHTML = data[9];
+            if(data[0].player1Score <= 170 && data[0].player1Score > 0){
+                document.getElementById(`out${data[0].gameID}1`).innerHTML = data[1];
             }else{
-                document.getElementById(`out${data[0]}1`).innerHTML = '';
+                document.getElementById(`out${data[0].gameID}1`).innerHTML = '';
             }
         
-            if(data[3] <= 170 && data[3] > 0){
-                document.getElementById(`out${data[0]}2`).innerHTML = data[10];
+            if(data[0].player2Score <= 170 && data[0].player2Score > 0){
+                document.getElementById(`out${data[0].gameID}2`).innerHTML = data[2];
             }else{
-                document.getElementById(`out${data[0]}2`).innerHTML = '';
+                document.getElementById(`out${data[0].gameID}2`).innerHTML = '';
             }
         break;
         case 1:
             numActiveGames += 1;
             let div = document.getElementById('activeTablesDiv1');
             $(document.getElementById('noActiveGames')).remove();
-            $(div).append(`<table id="${data[0]}" class="activeGameTable"><tr><td id="activePlayer${data[0]}1">${data[11]}</td><td><i id="${data[0]}TurnArrow" class="material-icons turnArrow">expand_more</i></td><td id="activePlayer${data[0]}2">${data[12]}</td></tr><tr><td id="activeDarts${data[0]}1"></td><td>Darts</td><td id="activeDarts${data[0]}2"></td></tr><tr><td id="activeLeg${data[0]}1">${data[2]}</td><td>Legs</td><td id="activeLeg${data[0]}2">${data[4]}</td></tr><tr><td id="activeScore${data[0]}1">${data[1]}</td><td></td><td id="activeScore${data[0]}2">${data[3]}</td></tr><tr><td id="out${data[0]}1"></td><td></td><td id="out${data[0]}2"></td></tr></table>`)
+            $(div).append(`<table id="${data[0].gameID}" class="activeGameTable"><tr><td id="activePlayer${data[0].gameID}1">${data[3]}</td><td><i id="${data[0].gameID}TurnArrow" class="material-icons turnArrow">expand_more</i></td><td id="activePlayer${data[0].gameID}2">${data[4]}</td></tr><tr><td id="activeDarts${data[0].gameID}1"></td><td>Darts</td><td id="activeDarts${data[0].gameID}2"></td></tr><tr><td id="activeLeg${data[0].gameID}1">${data[0].player1LegsWon}</td><td>Legs</td><td id="activeLeg${data[0].gameID}2">${data[0].player2LegsWon}</td></tr><tr><td id="activeScore${data[0].gameID}1">${data[0].player1Score}</td><td></td><td id="activeScore${data[0].gameID}2">${data[0].player2Score}</td></tr><tr><td id="out${data[0].gameID}1"></td><td></td><td id="out${data[0].gameID}2"></td></tr></table>`)
         
-            document.getElementById(`activeLeg${data[0]}1`).innerHTML = data[2];
-            document.getElementById(`activeLeg${data[0]}2`).innerHTML = data[4];
-            document.getElementById(`activeScore${data[0]}1`).innerHTML = data[1];
-            document.getElementById(`activeScore${data[0]}2`).innerHTML = data[3];
-            document.getElementById(`activeDarts${data[0]}1`).innerHTML = data[7];
-            document.getElementById(`activeDarts${data[0]}2`).innerHTML = data[8];
+            document.getElementById(`activeLeg${data[0].gameID}1`).innerHTML = data[0].player1LegsWon;
+            document.getElementById(`activeLeg${data[0].gameID}2`).innerHTML = data[0].player2LegsWon;
+            document.getElementById(`activeScore${data[0].gameID}1`).innerHTML = data[0].player1Score;
+            document.getElementById(`activeScore${data[0].gameID}2`).innerHTML = data[0].player2Score;
+            document.getElementById(`activeDarts${data[0].gameID}1`).innerHTML = data[0].player1DartsThrown;
+            document.getElementById(`activeDarts${data[0].gameID}2`).innerHTML = data[0].player2DartsThrown;
         
-            if(data[6] == '0'){
-                document.getElementById(`activePlayer${data[0]}1`).style.color = 'green';
-                document.getElementById(`activePlayer${data[0]}2`).style.color = 'white';
+            if(data[0].startingPlayer == '0'){
+                document.getElementById(`activePlayer${data[0].gameID}1`).style.color = 'green';
+                document.getElementById(`activePlayer${data[0].gameID}2`).style.color = 'white';
             }else{
-                document.getElementById(`activePlayer${data[0]}1`).style.color = 'white';
-                document.getElementById(`activePlayer${data[0]}2`).style.color = 'green';
+                document.getElementById(`activePlayer${data[0].gameID}1`).style.color = 'white';
+                document.getElementById(`activePlayer${data[0].gameID}2`).style.color = 'green';
             }
         
-            if(data[5] == 'true'){
-                document.getElementById(`${data[0]}TurnArrow`).style.transform = 'rotate(90deg)';
+            if(data[0].player1Turn == 'true'){
+                document.getElementById(`${data[0].gameID}TurnArrow`).style.transform = 'rotate(90deg)';
             }else{
-                document.getElementById(`${data[0]}TurnArrow`).style.transform = 'rotate(270deg)';
+                document.getElementById(`${data[0].gameID}TurnArrow`).style.transform = 'rotate(270deg)';
             }
         
-            if(data[1] <= 170 && data[1] > 0){
-                document.getElementById(`out${data[0]}1`).innerHTML = data[9];
+            if(data[0].player1Score <= 170 && data[0].player1Score > 0){
+                document.getElementById(`out${data[0].gameID}1`).innerHTML = data[1];
+                console.log(data[1])
             }else{
-                document.getElementById(`out${data[0]}1`).innerHTML = '';
+                document.getElementById(`out${data[0].gameID}1`).innerHTML = '';
             }
         
-            if(data[3] <= 170 && data[3] > 0){
-                document.getElementById(`out${data[0]}2`).innerHTML = data[10];
+            if(data[0].player2Score <= 170 && data[0].player2Score> 0){
+                document.getElementById(`out${data[0].gameID}2`).innerHTML = data[2];
+                console.log(data[2])
             }else{
-                document.getElementById(`out${data[0]}2`).innerHTML = '';
+                document.getElementById(`out${data[0].gameID}2`).innerHTML = '';
             }
         break;
         case 2:
@@ -124,9 +126,9 @@ function updateGames(data, infoType){
 
 function initiateActiveGames(data){
     let div = document.getElementById('activeTablesDiv1');
-    numActiveGames = data.length;
-
-    for(let i = 0; i < data.length; i++){
+    numActiveGames = data[0].length;
+    console.log(data);
+    for(let i = 0; i < data[0].length; i++){
         $(div).empty();
         $(div).append(`<table id="${data[i][0+i]}" class="activeGameTable"><tr><td id="activePlayer${data[i][0+i]}1">${data[i][11+i]}</td><td><i id="${data[i][0+i]}TurnArrow" class="material-icons turnArrow">expand_more</i></td><td id="activePlayer${data[i][0+i]}2">${data[i][12+i]}</td></tr><tr><td id="activeDarts${data[i][0+i]}1"></td><td>Darts</td><td id="activeDarts${data[i][0+i]}2"></td></tr><tr><td id="activeLeg${data[i][0+i]}1">${data[i][2+i]}</td><td>Legs</td><td id="activeLeg${data[i][0+i]}2">${data[i][4+i]}</td></tr><tr><td id="activeScore${data[i][0+i]}1">${data[i][1+i]}</td><td></td><td id="activeScore${data[i][0+i]}2">${data[i][3+i]}</td></tr><tr><td id="out${data[i][0+i]}1"></td><td></td><td id="out${data[i][0+i]}2"></td></tr></table>`)
 
@@ -169,7 +171,7 @@ function makePoules(pouleData){
     console.log(pouleData);
     var poulesDiv = document.getElementById('centerPoulesDiv');
 
-    for(let i = 0; i < pouleData.length; i++){
+    for(let i = 0; i < pouleData[0].length; i++){
         var pouleNum;
         switch(i){
             case 0:
